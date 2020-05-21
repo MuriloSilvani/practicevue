d519e409c82650fc56f474756c87cc374deb6fd5
Murilo <murilo.silvani@gmail.com>
2020-05-21 12:20:58 -0300

    +

--- src/components/Contador.vue
+++ src/components/Contador.vue
@@ -2,14 +2,14 @@
   <div>
     <span>total: {{ contador }}</span>
 
-    <button v-on:click="decrementar()">-</button>
-    <button v-on:clickcd="acrescentar()">+</button>
+    <button class="button" v-on:click="decrementar()">-</button>
+    <button class="button" v-on:click="acrescentar()">+</button>
 
     <section>
       <input v-model="input" />
-      <p>{{ input }}</p>
+      <!-- <p>{{ input }}</p> -->
 
-      <button v-if="input">Entrar</button>
+      <button class="button" @click="alertar()" v-if="input">Entrar</button>
     </section>
   </div>
 </template>
@@ -19,7 +19,8 @@ export default {
   data() {
     return {
       contador: 0,
-      input: undefined
+      input: undefined,
+      nome: "joao"
     };
   },
   methods: {
@@ -28,10 +29,36 @@ export default {
     },
     decrementar() {
       this.contador--;
+    },
+    alertar() {
+      alert(this.input + " Legal");
     }
+  },
+  beforeUpdate() {
+    console.log("Atualizando");
+  },
+  updated() {
+    console.log(" contador: " + this.contador + ", atualizado");
+  },
+  created() {
+    // setInterval(() => {
+    // this.contador++;
+    // }, 1500);
+  },
+  destroyed() {
+    console.log(this);
   }
 };
 </script>
 
 <style scoped>
+.button {
+  background: rgba(240, 50, 50, 1);
+  border: none;
+  border-radius: 5px;
+  color: #fff;
+  padding: 10px 25px;
+  margin: 5px 10px;
+  font-weight: bold;
+}
 </style>
